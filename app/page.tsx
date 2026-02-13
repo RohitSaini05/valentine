7c313cf6bec48eb3005584cf68781e98fbe51091
RohitSaini05 <r7665415553@gmail.com>
2026-02-14 00:49:19 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { HeartFilledIcon } from "@/components/ui/icons";
 import { Meteors } from "@/components/ui/meteors";
 import { SparklesCore } from "@/components/ui/sparkles";
@@ -7,7 +8,7 @@ import Link from "next/link";
 import { BsHeartbreak } from "react-icons/bs";
 import { GiHeartNecklace } from "react-icons/gi";
 
-function Valentine() {
+function valentine() {
   return (
     <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <div className="w-full absolute inset-0 h-screen">
@@ -21,54 +22,62 @@ function Valentine() {
           particleColor="#FFFFFF"
         />
       </div>
+      <div>
+        <div className="">
+          <div className=" h-3/4 md:h-1/2 w-full  relative ">
+            <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-pink-500 to-orange-400 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+            <div className="relative shadow-xl bg-black border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+              <GiHeartNecklace size={"50"} />
 
-      <div className="h-3/4 md:h-1/2 w-full relative">
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-pink-500 to-orange-400 transform scale-[0.80] rounded-full blur-3xl" />
+              <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+                Will You Be My Valentine?
+              </h1>
 
-        <div className="relative shadow-xl bg-black border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-          <GiHeartNecklace size={50} />
-
-          <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-            Will You Be My Valentine?
-          </h1>
-
-          <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-            I know we&apos;ve only just met
-            <br />
-            but you&apos;ve been popping into my thoughts in the nicest way.
-            <br />
-            Can&apos;t wait to see you,
-            <br />
-            Will you go out to the show with me?
-          </p>
-
-          <div className="flex gap-3 justify-between items-center">
-            <Button
-              as={Link}
-              className="text-sm font-normal text-default-600 bg-default-100"
-              href="/will/you"
-              startContent={<HeartFilledIcon className="text-danger" />}
-              variant="flat"
-            >
-              Yes I will
-            </Button>
-
-            <Button
-              as={Link}
-              className="text-sm font-normal text-default-600 bg-default-100 hover:bg-opacity-0 hover:scale-50 duration-75 ease-in-out"
-              href="/will/no"
-              startContent={<BsHeartbreak size={22} className="text-danger" />}
-              variant="flat"
-            >
-              No I won&apos;t
-            </Button>
+              <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
+                I know we've ony just met
+                <br></br>
+                but you’ve been popping into my thoughts in the nicest way.
+                <br></br>
+                Can't wait to see you,
+                <br></br>
+                Will you go out to the show with me?
+                <br></br>
+              </p>
+              <div className="flex gap-3 justify-between items-center">
+                <div>
+                  {" "}
+                  <Button
+                    as={Link}
+                    className="text-sm font-normal text-default-600 bg-default-100 "
+                    href={"/will/you"}
+                    startContent={<HeartFilledIcon className="text-danger" />}
+                    variant="flat"
+                  >
+                    Yes I will
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    as={Link}
+                    className="text-sm font-normal text-default-600 bg-default-100 hover:bg-opacity-0 hover:transform hover:scale-x-50 hover:scale-y-50 duration-75 ease-in-out"
+                    href={"/will/no"}
+                    startContent={
+                      <BsHeartbreak size={"22"} className="text-danger" />
+                    }
+                    variant="flat"
+                  >
+                    No I won&apos;t
+                  </Button>
+                </div>
+              </div>
+              {/* Meaty part - Meteor effect */}
+              <Meteors number={10} />
+            </div>
           </div>
-
-          <Meteors number={10} />
         </div>
       </div>
     </div>
   );
 }
 
-export default Valentine;
+export default valentine;
